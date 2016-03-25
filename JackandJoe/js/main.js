cdb8bf7ea3dcb3c63f7dc0d7a983f2162a35d36c
laurendpoole <Lauren@Laurens-MBP.home>
2016-03-25 19:08:44 -0400

    Final

---
+++ JackandJoe/js/main.js
@@ -0,0 +1,27 @@
+// $(document).ready(function() {
+
+//   $(window).scroll(function () {
+//       //if you hard code, then use console
+//       //.log to determine when you want the 
+//       //nav bar to stick.  
+//       console.log($(window).scrollTop())
+//     if ($(window).scrollTop() > 300) {
+//       $('.navigation').addClass('navigationScroll');
+//     }
+//     if ($(window).scrollTop() < 300) {
+//       $('.navigation').removeClass('navigationScroll');
+//     }
+//   });
+// });
+
+
+$(document).ready(function() {
+
+<script>
+    lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    })
+</script>
+
+});
