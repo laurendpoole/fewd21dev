08dd567cbed79d410124d234997c176b8547af44
laurendpoole <Lauren@Laurens-MBP.home>
2016-03-28 20:07:10 -0400

    edits

--- JackandJoe/js/main.js
+++ JackandJoe/js/main.js
@@ -1,27 +1,22 @@
-// $(document).ready(function() {
-
-//   $(window).scroll(function () {
-//       //if you hard code, then use console
-//       //.log to determine when you want the 
-//       //nav bar to stick.  
-//       console.log($(window).scrollTop())
-//     if ($(window).scrollTop() > 300) {
-//       $('.navigation').addClass('navigationScroll');
-//     }
-//     if ($(window).scrollTop() < 300) {
-//       $('.navigation').removeClass('navigationScroll');
-//     }
-//   });
-// });
-
-
 $(document).ready(function() {
+	console.log("hellooo, it's me")
 
-<script>
-    lightbox.option({
-      'resizeDuration': 200,
-      'wrapAround': true
-    })
-</script>
+		$(".fancybox").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		padding: 0 // remove white border
+	});
+
+    var mySwiper = new Swiper ('.swiper-container', {
+       pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    })   
 
 });
+
+	
